2b9d5016567d74a91a3c8555eb3e4b31bd1d9dd1
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-08-22 10:16:04 -0300

    criando um js para cada função

---
+++ assets/app/rules.js
@@ -0,0 +1,9 @@
+const rules_button = document.querySelector('#open_rules');
+const rules_section = document.querySelector('#rules');
+const rules_close = document.querySelector('#close_rules')
+rules_button.addEventListener("click",()=>{
+    rules_section.classList.remove('oculto');
+})
+rules_close.addEventListener("click",()=>{
+    rules_section.classList.add('oculto');
+})
