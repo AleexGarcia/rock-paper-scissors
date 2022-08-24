035fe4f2f4f8bc3e11021937343d698c8d160b64
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-08-24 13:29:46 -0300

    z-index resultado

--- assets/app/playAgain.js
+++ assets/app/playAgain.js
@@ -6,4 +6,6 @@ playAgain.addEventListener('click',()=>{
     resultHtml.classList.add('oculto');
     resultHtml.classList.remove('opacity');
     machinePick.classList.replace(machinePick.classList[0],`pick__machine`)
+    auraMachine.classList.remove('aura');
+    auraYou.classList.remove('aura');
 })
